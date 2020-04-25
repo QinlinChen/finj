5097f2d97c0f48c75a491168812cce60dc6d64da
ray <chenqinlin98@gmail.com>
2020-04-25 09:32:37 +0800

    rename count-fault.sh

--- tools/parse-log.sh
+++ tools/parse-log.sh
@@ -5,4 +5,4 @@ if [ "$#" -lt "1" ]; then
 fi
 
 sed 's/^\[FATAL.*\](\(.*\))Catch \(.*\) from \(.*\)$/\2 \1 \3/g' $1 \
-    | sed 's/\.sh\...../\.sh/g' | sort -u | wc -l
+    | sed 's/\.sh\...../\.sh/g' | sort -u
